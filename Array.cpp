580c79b8649183028a23c0cf98d2bcf6dc931654
kashishrathod7137 <kashishrathod0045@gmail.com>
2026-03-27 13:49:04 +0530

    Add files via upload

---
+++ Array.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+           int arrays[5] = {10, 20, 30, 40, 50};
+           int i;
+           
+           printf("Elements of the array are:\n");
+           
+           for(i = 0; i < 5; i++) {
+           	 printf("array[%d] = %d\n", i, array[i]);
+           }	
+           
+           return 0;
+}
