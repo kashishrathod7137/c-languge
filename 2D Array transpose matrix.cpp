71caf742ed671f35190d4a8f05a9b35aa4a6cfb0
kashishrathod7137 <kashishrathod0045@gmail.com>
2026-05-15 13:03:48 +0530

    Add files via upload

---
+++ 2D Array transpose matrix.cpp
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main()
+{
+     int matrix[10][10], transpose[10][10];
+     int rows, cols, i, j;
+     
+     printf("Enter number of rows and columns of the matrix:");
+     scanf("%d %d", &rows, &cols);
+     
+     printf("Enter elements of matrix:\n");
+     for(i = 0; i < rows; i++)
+     {
+     	for(j = 0; j < cols; j++)
+     	{
+                printf("Elements [%d][%d]:", i, j);
+                scanf("%d", &matrix[i][j]);
+  }
+      }
+	 
+	 printf("\n Original Matrix\n");
+	 for(i=0;i<rows;i++){
+ 	     for(j=0;j<cols;j++){
+ 	     	printf("%d\t" ,matrix[i][j]);
+	      }
+	      printf("\n");
+	 }    
+           for(i = 0; i< rows; i++)
+           {
+           	for(j = 0; j< cols; j++)
+          {
+          	transpose[j][i] = matrix[i][j];
+          }
+}
+            printf("\nTranspose Matrix:\n");
+            for(i = 0; i < cols; i++){
+            	for(j = 0; j < rows; j++){
+            		printf("%d\t" , transpose[i][j]);
+	            }
+	            printf("\n");
+            }
+            return 0;
+}
