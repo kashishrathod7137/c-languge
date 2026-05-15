d38c40f1514300bd74ad40c8675fddd94bc4a10b
kashishrathod7137 <kashishrathod0045@gmail.com>
2026-05-15 12:44:42 +0530

    Add files via upload

---
+++ one dimesnsion ds 02.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	int array [6] ={10,20,30,40,50,80};
+	int i;
+	printf("elements of the array are :\n");
+	
+	for (i =0; i <6
+; i++) {
+		printf("array[%d] = %d\n" ,i, array[i]);
+	}
+return 0;
+}
---
+++ pointer array.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+	int array[5] = {10, 20, 30, 40, 50};
+	int *ptr;
+	int i;
+	
+	ptr = array;
+	printf("Accessing array elements using pointers:\n");
+	for(i = 0; i < 5; i++)
+	{
+		printf("Elements %d = %d\n", i, *(ptr + i));
+	}
+	return 0;
+}
